aea0756dd76730b99bfdbd5ca1118a069f676c82
Alex Aichinger <aichinger.alex24315@gmail.com>
2017-03-15 20:33:17 -0700

    Messing with ExpressJS, explained setup in README, updated gitignore to ignore VSCode file

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Welcome to Tank City
 
 
 
-
 This is our term project created by Ivan, Vivian, Maroun, and Alex for CSC 667.
+Setup:
+1. npm install
+2. DEBUG=term-project-spring-2017-ivm_:* npm start
+3. open browser localhost:3000/
--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Sets up to route to proper locations
 var index = require('./routes/index');
-var users = require('./routes/users');
+var profile = require('./routes/profile');
+var scoreboard = require('./routes/scoreboard');
+var register = require('./routes/register');
+
 
 var app = express();
 
@@ -15,32 +19,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// This is where the routing happens
 app.use('/', index);
-app.use('/users', users);
+app.use('/profile', profile);
+app.use('/scoreboard', scoreboard);
+app.use('/register', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
