9a57c8297f3367a649c55bfdeb3a0a28c665230c
ivanw-yu <ivnyu@mail.sfsu.edu>
2017-04-07 03:19:16 -0700

    implemented simple registration, successful registration now redirects user to homepage containing message for complete registration

--- routes/register.js
+++ routes/register.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('.././server/models/db.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('register', { title: 'Register' });
+});
+
+router.post('/', function(req, res, next) {
+    const user = req.body;
+    db.query('INSERT INTO Player (first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5);'
+                  ,[user.first_name, user.last_name, user.username, user.email, user.password]);
+    // the url will have a query string registration=complete
+    // in index.js, having this query string will print 'Registration Successful' in the rendered page.
+    res.redirect('../?registration=complete');
 });
 
 module.exports = router;
