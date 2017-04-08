cb6081d440e060b850d73a0c45d2ac98c83a73de
ivanw-yu <ivnyu@mail.sfsu.edu>
2017-04-07 22:05:55 -0700

    registration updated, users can no longer register usernames and emails that has already been taken, and forms can no longer be submitted with blank fields

--- routes/register.js
+++ routes/register.js
@@ -10,39 +10,24 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
     const user = req.body;
     // if any of the fields in the submitted form is blank, redirect to the same page but with an error message.
-    if(!user.first_name || !user.last_name || !user.email || !user.password || !user.username) {
-      res.render('register', { title: 'Register', invalid_registration: ' All fields required. ' });
-    }else if(user.passwordConfirm !== user.password) {
+    if(user.passwordConfirm !== user.password) {
       res.render('register', { title: 'Register', invalid_registration: ' Passwords did not match. ' });
     }else{
 
-      /*let result = db.query('SELECT username FROM Player WHERE username = \'' + user.username + '\'');
-      console.log("Query result: " + result);
-      //if(!result.rows.length)
-        //res.render('register', { title: 'Register', invalid_registration: ' The username is already taken. ' });
-      result = db.query('SELECT email FROM Player WHERE email = \''  + user.email + '\'');
-      console.log("Query result: " + result);
-
-      //if(!result.rows.length)
-        //res.render('register', { title: 'Register', invalid_registration: ' The email is already associated with an account. ' });
-
-      /*result.on('row', function(row) {
-        if(row.username === user.username) {
-          res.render('register', { title: 'Register', invalid_registration: ' The username is already taken. ' });
-        }
-      });
-      result = db.query('SELECT email FROM Player WHERE email = '  + user.email);
-      result.on('row', function(row) {
-        if(row.email === user.username) {
-          res.render('register', { title: 'Register', invalid_registration: ' The email is already associated with an account. ' });
-        }
-      });*/
-
-      db.query('INSERT INTO Player (first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5);'
-                  ,[user.first_name, user.last_name, user.username, user.email, user.password]);
-    // the url will have a query string registration=complete
-    // in index.js, having this query string will print 'Registration Successful' in the rendered page.
-      res.redirect('../?registration=complete');
+      db.query('INSERT INTO Player (first_name, last_name, username, email, password) '
+              + 'VALUES ( \'' + user.first_name +'\', \'' + user.last_name + '\', \'' + user.username
+              + '\', \'' + user.email + '\', \'' + user.password + '\');',
+              function(err, result){
+                // if insertion fails because it violated constraints (like the constraint that
+                // all password and username should be unique), then the error message will be displayed
+                if(err){
+                  res.render('register', { title: 'Register', invalid_registration: ' Username or email already taken. ' });
+                }else {
+                  // if registration is successful, go back to the homepage, and the url will have a query string registration=complete
+                  // in index.js, having this query string will print 'Registration Successful' in the rendered page.
+                  res.redirect('../?registration=complete');
+                }
+              });
     }
 });
 
