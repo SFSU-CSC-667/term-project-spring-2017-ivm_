241b9036beab950b8b66434f43764d8beefd3af6
Maroun Abi ramia <marounab27@gmail.com>
2017-04-15 12:46:53 -0700

    began log in verification for scoreboard page

--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+var db = require('.././server/models/db.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('scoreboard', { title: 'Scoreboard' });
+router.get('/', passport.authenticate('bearer', { session: true }),function(req, res, next) {
+    db.query('SELECT first_name, wins FROM Player;', function(error, result){
+        if (error){
+            res.render('scoreboard', { title: 'Scoreboard' });
+            return;
+        }
+        res.render('scoreboard', { title: 'Scoreboard' });
+    });
 });
 
 module.exports = router;
