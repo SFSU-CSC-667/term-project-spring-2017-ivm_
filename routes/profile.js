639b98f182df92a7890eb6782e213c36b57459c1
Vivian Lee <vivianlee8295@gmail.com>
2017-04-15 12:48:43 -0700

    Migrate to pug

--- routes/profile.js
+++ routes/profile.js
@@ -4,7 +4,7 @@ var db = require('.././server/models/db.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    // allows all users to be rendered in profile.jade
+    // allows all users to be rendered in profile.pug
     var result = db.query('SELECT * FROM Player;', function(err, result){
       //res.send(result.rows);
       res.render('profile', {query_rows: result.rows});
