7939744420fb33cf2e4bebf2472f8a6e2107baf0
Maroun Abi ramia <marounab27@gmail.com>
2017-04-15 14:17:17 -0700

    scoreboard page displays results

--- model/scoreboard.js
+++ model/scoreboard.js
@@ -1,11 +1,10 @@
 var db = require('.././server/models/db.js');
 
 exports.getScores = function(dataReady){
-    db.query('SELECT * FROM Player;', function(error, result){
+    db.query('SELECT first_name, wins FROM Player;', function(error, result){
         if (error){
             console.log('Error loading scoreboard. ' + error.status);
         }
-        console.log("Here : " + result);
         dataReady(result, error);
     });
 };
--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -7,11 +7,26 @@ router.get('/',function(req, res, next) {
     sb.getScores(function(result, error){
         if (error){
             console.log("error: " + error.statusCode);
+            res.render('scoreboard', { title: 'Scoreboard' });
         }
-        console.log("my board: " + result);
-    });
 
-    res.render('scoreboard', { title: 'Scoreboard' });
+        logScoreboard(result.rows);
+        res.render('scoreboard', { scores: result.rows });
+    });
 });
+
+function logScoreboard(scores){
+    console.log("==========================================");
+    console.log("============= ( SCOREBOARD ) =============");
+    console.log("==========================================");
+
+    console.log("Name\t\t\tWins");
+
+    for (var s = 0; s < scores.length; s++) {
+        console.log(scores[s]["first_name"] + "\t\t\t" + scores[s]["wins"]);
+    }
+
+    console.log("==========================================");
+}
 
 module.exports = router;
