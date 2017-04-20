9681077be2407bfe5bc4190bfac4a37a43938761
ivanw-yu <ivnyu@mail.sfsu.edu>
2017-04-20 16:53:26 -0700

    player model now has functions that used for finding users using email or username, getting all users, and registering users

--- model/player.js
+++ model/player.js
@@ -1,4 +1,8 @@
 var db = require('.././server/models/db.js');
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+
+
 
 exports.registerNewPlayer = function(res, reqBody, callBack) {
   const user = reqBody.body;
@@ -20,6 +24,52 @@ exports.registerNewPlayer = function(res, reqBody, callBack) {
             });
   }
 
+
+exports.loginPlayer = function(reqBody, callBack) {
+  const user = reqBody.body;
+  // if any of the fields in the submitted form is blank, redirect to the same page but with an error message.
+  console.log("here");
+  db.query('SELECT * FROM Player WHERE email = \'' + user.email + '\' AND password = \'' + user.password + '\';',
+            function(err, result){
+
+              if(err || result.rows.length !==1){
+                console.log(result.rows);
+                err = err || 1;
+                callBack(err, null);
+              }else {
+                console.log(result.rows);
+                callBack(0, result.rows[0]);
+              }
+            });
+  }
+
+exports.findByUsername = function(username, callBack){
+    db.query('SELECT * FROM Player WHERE username = \'' + username + '\';',
+            function(err, result){
+              // if(result.rows.length!== 1 || err) return callBack(null);
+              // else return callBack(result.rows[0]); // else return the 1 and only row
+              if(err){ console.log("player.js error");return callBack(err);}
+              else if(result.rows.length!== 1){ return callBack(0, null);}
+              else return callBack(1, result.rows[0]);
+            });
+}
+
+exports.findByEmail = function(username, callBack){
+    db.query('SELECT * FROM Player WHERE email = \'' + email + '\';',
+            function(err, result){
+              if(err) return callBack(err);
+              else if(result.rows.length!== 1) return callBack(null);
+              else return callBack(result.rows[0]); // else return the 1 and only row
+            });
+}
+
+exports.getAll = function(callBack){
+    db.query('SELECT * FROM Player;',
+            function(err, result){
+              if(err) return callBack(err);
+              else return callBack(result.rows); // else return the 1 and only row
+            });
+}
 
 /*exports.getAllPlayer = function(getAll) {
   var result = db.query('SELECT * FROM Player;', function(err, result){
