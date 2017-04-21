a69c5dd50bb7193ce01d7dcb79c0659c3a1fab2c
Maroun Abi ramia <marounab27@gmail.com>
2017-04-20 17:18:58 -0700

    implmenting chat model

--- model/chat.js
+++ model/chat.js
@@ -1,3 +1,20 @@
-/**
- * Created by Maroun on 3/21/17.
- */
+var db = require('.././server/models/db.js');
+
+exports.getLobbyChats = function(dataStream){
+    db.query('SELECT player_id, message FROM Chat WHERE chat_id = 1;', function(error, result){
+        if (error){
+            console.log("getLobbyChats error: " + error);
+        }
+        dataStream(error, result);
+    })
+};
+
+exports.getChatById = function(dataStream){
+    db.query('SELECT ', function(error, result){
+        if (error){
+            console.log('getChatById error: ' + error);
+        }
+        dataStream(error, result);
+    });
+};
+
