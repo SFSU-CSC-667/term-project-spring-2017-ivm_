c139b4e569f546b8309cf55d87fd2bcf5f4447d1
ivanw-yu <ivnyu@mail.sfsu.edu>
2017-04-21 01:57:37 -0700

    methods used by all routes are placed in user.js

--- routes/profile.js
+++ routes/profile.js
@@ -5,9 +5,10 @@ module.exports = function(app, passport){
 
   var express = require('express');
   var router = express.Router();
+  var user = require('.././user.js');
   var db = require('.././server/models/db.js');
 
-  router.get('/', isLoggedIn, function(req, res, next) {
+  router.get('/', user.isLoggedIn, function(req, res, next) {
       // allows all users to be rendered in profile.pug
       var result = db.query('SELECT * FROM Player;', function(err, result){
         res.render('profile', {query_rows: result.rows, userName: req.user.username});
@@ -17,12 +18,12 @@ module.exports = function(app, passport){
 }
 
 // this checks if the user is logged in, if not, it redirects to homepage.
-function isLoggedIn(req, res, next) {
-
-    if (req.isAuthenticated()){
-        console.log(req.user.username);
-        return next();
-    }
-
-    res.redirect('/');
-}
+// function isLoggedIn(req, res, next) {
+//
+//     if (req.isAuthenticated()){
+//         console.log(req.user.username);
+//         return next();
+//     }
+//
+//     res.redirect('/');
+// }
