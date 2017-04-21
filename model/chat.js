598a085bcc2fe119980320062c3684a84a0b73f0
Maroun Abi ramia <marounab27@gmail.com>
2017-04-20 19:12:52 -0700

    getAllChatsWithId implemented

--- model/chat.js
+++ model/chat.js
@@ -1,19 +1,24 @@
 var db = require('.././server/models/db.js');
 
+//1
 exports.getLobbyChats = function(dataStream){
-    db.query('SELECT player_id, message FROM Chat WHERE chat_id = 1;', function(error, result){
+    db.query('SELECT player_id, message FROM Chat WHERE game_id=' + 1 + ';', function(error, result){
+
         if (error){
             console.log("getLobbyChats error: " + error);
         }
+
         dataStream(error, result);
     })
 };
 
-exports.getChatById = function(dataStream){
-    db.query('SELECT ', function(error, result){
+exports.getAllChatsWithId = function(cid, dataStream){
+    db.query('SELECT * FROM Chat WHERE chat_id=' + cid, function(error, result){
+
         if (error){
             console.log('getChatById error: ' + error);
         }
+
         dataStream(error, result);
     });
 };
