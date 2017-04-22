a878d41000d886e3e55f856efafc797734701cdd
Alex Aichinger <aichinger.alex24315@gmail.com>
2017-04-21 19:15:12 -0700

    Finished rudimentary scoreboard with results being displayed in decreasing order. Limited testing

--- routes/profile.js
+++ routes/profile.js
@@ -1,21 +1,18 @@
+module.exports = function(app, passport) {
 
+    var express = require('express');
+    var router = express.Router();
+    var user = require('.././user.js');
+    var db = require('.././server/models/db.js');
 
-
-module.exports = function(app, passport){
-
-  var express = require('express');
-  var router = express.Router();
-  var user = require('.././user.js');
-  var db = require('.././server/models/db.js');
-
-  router.get('/', user.isLoggedIn, function(req, res, next) {
-      // allows all users to be rendered in profile.pug
-      var result = db.query('SELECT * FROM Player;', function(err, result){
-        if(err) console.log(err);
-        res.render('profile', {query_rows: result.rows, userName: req.user.username});
-      });
-  });
-  return router;
+    router.get('/', user.isLoggedIn, function(req, res, next) {
+        // allows all users to be rendered in profile.pug
+        var result = db.query('SELECT * FROM Player;', function(err, result) {
+            if (err) console.log(err);
+            res.render('profile', { query_rows: result.rows, userName: req.user.username });
+        });
+    });
+    return router;
 }
 
 // this checks if the user is logged in, if not, it redirects to homepage.
--- routes/scoreboard.js
+++ routes/scoreboard.js
@@ -3,27 +3,36 @@ var router = express.Router();
 var sb = require('.././model/scoreboard.js');
 
 /* GET scoreboard page. */
-router.get('/',function(req, res, next) {
-    sb.getScores(function(result, error){
-        if (error){
+router.get('/', function(req, res, next) {
+    sb.getScores(function(result, error) {
+        if (error) {
             console.log("error: " + error.statusCode);
             res.render('scoreboard', { title: 'Scoreboard' });
         }
 
         logScoreboard(result.rows);
-        res.render('scoreboard', { scores: result.rows });
+
+        let scores = result.rows.sort(function(a, b) {
+            return b.wins - a.wins;
+        });
+
+        res.render('scoreboard', { scores: scores });
     });
 });
 
-function logScoreboard(scores){
+function logScoreboard(scores) {
     console.log("==========================================");
     console.log("============= ( SCOREBOARD ) =============");
     console.log("==========================================");
 
     console.log("Name\t\t\tWins");
 
-    for (var s = 0; s < scores.length; s++) {
-        console.log(scores[s]["first_name"] + "\t\t\t" + scores[s]["wins"]);
+    let score = scores.sort(function(a, b) {
+        return b.wins - a.wins;
+    });
+
+    for (var s = 0; s < score.length; s++) {
+        console.log(score[s]["first_name"] + "\t\t\t" + score[s]["wins"]);
     }
 
     console.log("==========================================");
