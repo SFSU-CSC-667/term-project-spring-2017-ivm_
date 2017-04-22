f00d5be406fb5da281f862c77000c5a876fbebf6
Vivian Lee <vivianlee8295@gmail.com>
2017-04-22 10:51:16 -0700

    Update to profile route to handle user details

--- routes/profile.js
+++ routes/profile.js
@@ -9,7 +9,9 @@ module.exports = function(app, passport){
       // allows all users to be rendered in profile.pug
       var result = db.query('SELECT * FROM Player WHERE username = \'' + req.user.username +'\';', function(err, result){
         if(err) console.log(err);
-        res.render('profile', {query_rows: result.rows, userName: req.user.username});
+        res.render('profile', {query_rows: result.rows, userName: req.user.username,
+                               name: req.user.first_name + ' ' + req.user.last_name,
+                               email: req.user.email});
       });
   });
   return router;
