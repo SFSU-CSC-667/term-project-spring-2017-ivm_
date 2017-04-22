3aec85ccac07b631a6fc574853580706238a7222
ivanw-yu <ivnyu@mail.sfsu.edu>
2017-04-22 13:46:49 -0700

    postgres configuration set up

--- server/db.js
+++ server/db.js
@@ -6,6 +6,20 @@ const pg = require('pg');
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/config.json')[env];
 
+if(process.env.DATABASE_URL) {
+  const configParameters = require('url').parse(process.env.DATABASE_URL);
+  const credentials = params.auth.split(':');
+  config = {
+    user: credentials[0],
+    password: credentials[1],
+    host: configParameters.hostname,
+    port: configParameters.port,
+    database: configParameters.pathname.split('/')[1],
+    ssl: true
+  }
+}
+
+
 const pool = new pg.Pool(config);
 
 var query = function(sql){
