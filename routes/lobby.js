33d161b032c4611bf715b8344948d8c28e4dbfbd
Alex Aichinger <aichinger.alex24315@gmail.com>
2017-04-23 11:58:53 -0700

    finished rough look of lobby page

--- routes/lobby.js
+++ routes/lobby.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var sb = require('.././model/scoreboard.js');
 
 router.get('/', function(req, res, next) {
-    if (req.isAuthenticated()){
-        res.render('lobby', { title: 'Tank City Lobby', user: req.user });
-    }else{
-        res.render('index', { error: 'Log in to start game!' });
+
+    if (!req.isAuthenticated()) {
+        res.render('index', { title: 'Home', error: "Must be logged in" });
     }
 
+    sb.getScores(function(result, error) {
+        if (error) {
+            console.log("error: " + error.statusCode);
+            res.render('lobby', { title: 'Lobby', scores: scores, user: req.user });
+        }
+
+        let scores = result.rows.sort(function(a, b) {
+            return b.wins - a.wins;
+        });
+
+        res.render('lobby', { scores: scores, user: req.user });
+    });
 });
 
 module.exports = router;
