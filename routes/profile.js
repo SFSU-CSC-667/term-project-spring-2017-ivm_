4d8337046548f1b5f4ee62c0df0b4f9e2ce0bf48
Vivian Lee <vivianlee8295@gmail.com>
2017-05-06 12:01:37 -0700

    Merge branches 'maroun' and 'vivian' of https://github.com/SFSU-CSC-667/term-project-spring-2017-ivm_ into vivian
    # Conflicts:
    #	app.js
    #	public/js/main.js
    #	routes/index.js
    #	routes/profile.js
    #	server/passport.js
    #	views/layout.pug
    #	views/lobby.pug

--- routes/profile.js
+++ routes/profile.js
@@ -7,10 +7,9 @@ module.exports = function(app, passport) {
     var db = require('.././server/db.js');
     var userID = 0;
 
-module.exports = function(app, passport){
-  const express = require('express');
-  const router = express.Router();
-  const user = require('.././user.js');
+    const express = require('express');
+    const router = express.Router();
+    const user = require('.././user.js');
 
     router.get('/', user.isLoggedIn, function (req, res, next) {
         // allows all users to be rendered in profile.pug
