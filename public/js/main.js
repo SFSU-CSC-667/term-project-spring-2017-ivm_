680a7f22bbe690827c843a905b6fd8ce0bf7de68
Maroun Abi ramia <marounab27@gmail.com>
2017-05-16 18:06:17 -0700

    update game lobby message error

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,23 @@
 var socket = io();
 
-$( ".send-message" ).click(function() {
-  socket.emit('user_message', $('#username').text() + ": " + $('.user-message-input').val());
+$( ".send-lobby-message" ).click(function() {
+  socket.emit('user_message', $('#username').text() + ": " + $('.user-lobby-input').val());
   console.log("inside lobby messages");
-  $.post( "/lobby", {message: $('.user-message-input').val()});
+  $.post( "/lobby", {message: $('.user-lobby-input').val()});
 
-  $('.user-message-input').val('');
+  $('.user-lobby-input').val('');
 
   return false;
 });
 
 socket.on('user_message', function( message ){
-  $( '#messages').append( "<tr><td>" + message + "</td></tr>" + '<br />')
+  $( '#lobby-messages').append( "<li>" + message + "</li>" + '<br />')
 });
 
 // for game - client
 $( ".game-send-message" ).click(function() {
     // if incoming game_id != url's game_id
     socket.emit('game_user_message', {game: $("#gameid").text(), message: $('#username').text() + ": " + $('.user-message-input').val()});
-
     $.post( "/game", {message: $('.user-message-input').val()});
 
     $('.user-message-input').val('');
